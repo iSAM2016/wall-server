e8befcbe98102ab707ae740278cfd5ab18818ee1
isam2016 <isam2016@126.com>
2020-02-07 19:51:25 +0800

    初步联调

--- commands/test/test.js
+++ commands/test/test.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-import * as shell from 'shelljs';
+var shell = require('shelljs');
 describe('测试命令行', function() {
   // var manage = new Manage();
   //   it('test => 任务调度', function() {
@@ -7,22 +7,38 @@ describe('测试命令行', function() {
   //   });
   // it('test =>sql调度', function() {
   //   shell.exec(
-  //     "node ../index.js Utils:GenerateSQL 13 '2020-01' '2020-12' > ../../../int.sql",
+  //     "node ../dist/index.js Utils:GenerateSQL 1,12 '2020-01' '2020-12' > ../../int.sql",
   //   );
   // });
-
   //   it('test =>测试读取nginx 日志', function() {
-  //     shell.exec(' NODE_ENV=test node ../index.js SaveLog:Nginx');
+  //     shell.exec(' NODE_ENV=test node ../dist/index.js SaveLog:Nginx');
   //   });
-  it('test =>测试解析uv(设备访问)日志', function() {
+  // it('test =>测试解析uv(设备访问)日志', function() {
+  //   shell.exec(
+  //     "NODE_ENV=test node ../dist/index.js Parse:UV '2020-02-07 07:21' '2020-02-07 07:21'",
+  //   );
+  // });
+  // it('test =>测试 用户停留时间', function() {
+  //   shell.exec(
+  //     "NODE_ENV=test node ../dist/index.js Parse:TimeOnSiteByHour '2020-02-07 07:21' '2020-02-07 07:21'",
+  //   );
+  // });
+  // it('test =>统计设备信息', function() {
+  //   shell.exec(
+  //     "NODE_ENV=test node ../dist/index.js Parse:Device '2020-02-07 07:21' '2020-02-07 07:21'",
+  //   );
+  // });
+  // it('test => 点击', function() {
+  //   shell.exec(
+  //     "NODE_ENV=test node ../dist/index.js Parse:MenuClick  '2020-02-07 07:21' '2020-02-07 07:21'",
+  //   );
+  // });
+  it('test => 测试用户第一次登陆', function() {
     shell.exec(
-      "NODE_ENV=test node ../index.js Parse:UV '2020-02-07 07:21' '2020-02-07 07:21'",
+      "NODE_ENV=test node ../dist/index.js Parse:UserFirstLoginAt   '2020-02-07 07:21' '2020-02-07 07:21'",
     );
   });
 });
 
-// 'Parse:UV',
-// 'Parse:TimeOnSiteByHour',
 // 'Parse:Performance',
 // 'Parse:Monitor',
-// 测试
